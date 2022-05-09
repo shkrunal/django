1507d25e3a229014790f9bc040a8b51e677b0f7f
Krunal Sharma <94603395+shkrunal@users.noreply.github.com>
2022-05-09 11:17:42 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import contact,fedback,calculator
-    
-urlpatterns = [  
-    path("contact/",contact,name='contact'),
-    path("feedback/",fedback,name='feedback'),
-    path('cal/',calculator,name='cal')
-]
