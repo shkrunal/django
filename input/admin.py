5222942070c41f96b282fc64d2d390dede055c45
Krunal Sharma <94603395+shkrunal@users.noreply.github.com>
2022-05-09 11:17:10 +0530

    Delete input directory

--- input/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-from .models import contect, feedback ,cal
-
-# Register your models here.
-admin.site.register(feedback)
-admin.site.register(contect)
-admin.site.register(cal)
